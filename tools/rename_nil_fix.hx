d09b076f05c7e10a240366a9d8cd9d2278e8eaff
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-12-02 06:48:13 -0500

    Create rename_nil_fix.hx

---
+++ tools/rename_nil_fix.hx
@@ -0,0 +1,42 @@
+import sys.FileSystem;
+import sys.io.File;
+
+class RenameNilFix {
+    public static function main() {
+        var dirs = [
+            "export/cpp/obj/include/thx/",
+            "export/cpp/obj/src/thx/",
+            "export/mac/obj/include/thx/",
+            "export/mac/obj/src/thx/"
+        ];
+
+        for (dir in dirs) {
+            if (FileSystem.exists(dir)) {
+                renameIfExists(dir + "Nil.h", dir + "ThxNil.h");
+                renameIfExists(dir + "Nil.cpp", dir + "ThxNil.cpp");
+
+                // Patch include statements
+                patchIncludes(dir + "ThxNil.h");
+                patchIncludes(dir + "ThxNil.cpp");
+            }
+        }
+    }
+
+    static function renameIfExists(oldPath:String, newPath:String) {
+        if (FileSystem.exists(oldPath)) {
+            FileSystem.rename(oldPath, newPath);
+        }
+    }
+
+    static function patchIncludes(path:String) {
+        if (!FileSystem.exists(path)) return;
+
+        var txt = File.getContent(path);
+        txt = StringTools.replace(txt, "Nil.h", "ThxNil.h");
+        txt = StringTools.replace(txt, "Nil.cpp", "ThxNil.cpp");
+        txt = StringTools.replace(txt, "Nil_obj", "ThxNil_obj");
+        txt = StringTools.replace(txt, "Nil", "ThxNil");
+
+        File.saveContent(path, txt);
+    }
+}
